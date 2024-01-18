1cba16041d0f069cd997646840c8b8e39849feaf
Ag-Utkarsh <utkarshagarwal9641@gmail.com>
2024-01-18 09:14:46 +0530

    Band Name Generator Added

---
+++ Band Name Generator/main.py
@@ -0,0 +1,9 @@
+print("Welcome to Band Name Generator!")
+print("Let's create a unique band name based on some fun inputs.\n")
+
+band = input("What's do you want to add in front of the name?\n")
+your_name = input("What's your name?\n")
+
+print("\nDrumroll, please...\n")
+print(f"Congratulations! Your band name could be '{band} {your_name}'!")
+print("Rock on!")
